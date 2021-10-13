6250d997a54dca807795eb90d61a3fa033e72acc
Hathor <koshknot@gmail.com>
2021-10-13 17:31:56 -0400

    commit tree a030231f37caa6b52e2fb8993e86151ed621ab1c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram 
+  def initialize word
+    @word = word
+  end
+  def match arr
+    # we need to comapre the elements in an array
+    # .chars creates an array with each letter from a  string
+    # .sort can also help by lining everything up
+    arr.each |string| if string.chars.sort == word.chars.sort
+    puts "#{string} is an anagram of #{word}"
+end
+
+end
